e448f2e9f4abb82919561b24d101d3a59e002c38
JwolffTeach <jwolff@dsdmail.net>
2018-01-03 13:30:33 -0700

    Lesson 4 - Minor changes to comments again .
    Why so many commits?

--- lesson4/spaceshooter.py
+++ lesson4/spaceshooter.py
@@ -3,6 +3,7 @@ Lesson 4 - Refactor screen updating to game_functions. Restrict the ship's movem
   Changes to this file:
     1. screen.fill, screen.blit, player.blitme and display.flip code was moved to the game_functions.py file.
     2. Add settings as a parameter to pass to player when we instantiate player.
+    3. Cleaned up comments in Main Program Loop.
 """
 
 import pygame
@@ -21,11 +22,6 @@ pygame.init()
 settings = Settings()
 screen = pygame.display.set_mode( (settings.screen_width, settings.screen_height) )
 
-background_image = pygame.image.load("Backgrounds/blue2.png").convert()
-
-# Load the sounds
-lazer = pygame.mixer.Sound("Bonus/sfx_laser1.ogg")
-
 player = Ship(screen, settings) # Create the player; Player is a ship; Draw the ship on the screen
 
 pygame.display.set_caption("My Game")
@@ -38,18 +34,16 @@ clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
 while not done:
-    # --- Main event loop
-    # set done to whatever gf.check_events returns. If check_events returns 
-    # True, that means the user is quitting the application.
-    done = gf.check_events(player)
     
-    # Check quit event
+    # --- Check events; If user quits, exit main program loop
+    done = gf.check_events(player)
     
     # --- Game logic should go here
     player.update_location(settings)
     
+    # --- Draw all objects to the screen
     gf.update_screen(screen, player)
- 
+    
     # --- Limit to 60 frames per second
     clock.tick(60)
  
